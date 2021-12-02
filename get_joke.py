ef2f6c4fa1ecd82c477871fb8b03ef1e420e80e9
sam@paimon <samuel.vidovich@gmail.com>
2021-12-02 01:14:19 -0500

    Add --cow option

--- get_joke.py
+++ get_joke.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 
+import argparse
+import cowsay
 import json
 import requests
 
 
 def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cow', action='store_true', required=False, help='A cow tells the joke.')
+    args = parser.parse_args()
+
     response = requests.get('https://icanhazdadjoke.com/', headers={'Accept': 'application/json', 'User-Agent': 'dad-joke-terminal'})
     joke: dict = json.loads(response.text)
     if 'joke' in joke.keys():
-        print('Dad says:')
-        print(f"\t{joke['joke']}")
+        joke = joke['joke']
+        if args.cow:
+            cowsay.cow(joke)
+        else:
+            print('Dad says:')
+            print(f"\t{joke}")
     else:
         print("Sorry champ, Dad just couldn't find a joke for you this time.")
     
